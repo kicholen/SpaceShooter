d464a10f82633771cf7c182bcfeecb849f47fac4
Mateusz Debski <mateuszdeb@gmail.com>
2016-02-06 13:36:51 +0100

    fixed dropDownElements, sort list before creatingOptions

--- Assets/EditorScript/View/Level/Hud/RightBottomPanel/RightBottomPanelViewUpdaterBase.cs
+++ Assets/EditorScript/View/Level/Hud/RightBottomPanel/RightBottomPanelViewUpdaterBase.cs
@@ -18,8 +18,8 @@ public class RightBottomPanelViewUpdaterBase {
 
     protected GameObject createDropdownElement(string infoText, string defaultText, List<string> names, UnityAction<string> onValueChange) {
         GameObject gameObject = UnityEngine.Object.Instantiate(Resources.Load<GameObject>("Prefab/UI/EditorView/Level/DropdownElement"));
+        names.Sort((x, y) => Convert.ToInt16(x).CompareTo(Convert.ToInt16(y)));
         List<Dropdown.OptionData> options = names.Select(name => new Dropdown.OptionData(name)).ToList<Dropdown.OptionData>();
-        options.Sort((x, y) => Convert.ToInt16(x.text).CompareTo(Convert.ToInt16(y.text)));
         gameObject.GetComponentInChildren<Dropdown>().options = options;
         gameObject.GetComponentInChildren<Dropdown>().value = names.IndexOf(defaultText);
         gameObject.transform.FindChild("Label").GetComponent<Text>().text = infoText;
