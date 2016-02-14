ea541e800ee653477910e1b75a41c940047024eb
Mateusz Debski <mateuszdeb@gmail.com>
2016-02-14 17:29:37 +0100

    refactor(tools): DownloadFromServer
    JellyTools, download jsons from server
    is based on promise

--- Assets/Editor/JellyTools.cs
+++ Assets/Editor/JellyTools.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RSG;
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -9,69 +10,134 @@ public class JellyTools {
     [MenuItem("Jelly/DownloadAll")]
     public static void DownloadAll() {
         EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneWindows64);
-        downloadPaths(savePaths);
-        downloadLevels(saveLevels);
-        downloadEnemies(saveEnemies);
+        Promise.All(
+            downloadPaths().Then(models => savePaths(models)),
+            downloadLevels().Then(models => saveLevels(models)),
+            downloadEnemies().Then(models => saveEnemies(models)),
+            downloadBonuses().Then(models => saveBonuses(models)),
+            downloadDifficulties().Then(models => saveDifficulties(models))
+        )
+        .Catch(exception => Debug.Log(exception.Message))
+        .Done(() => Debug.Log("Download Completed"));
     }
 
-    static void downloadPaths(Action<List<PathModelComponent>> onPathsLoaded) {
+    static IPromise<List<PathModelComponent>> downloadPaths() {//Action<List<PathModelComponent>> onPathsLoaded
+        Promise<List<PathModelComponent>> promise = new Promise<List<PathModelComponent>>();
         GetPaths request = new GetPaths();
         builder.Build(request);
         while (!request.Process().isDone) { }
 
         if (request.Successful()) {
             request.ParseResult();
-            onPathsLoaded(request.Paths);
+            promise.Resolve(request.Paths);
         }
         else {
-            onRequestFailed(request.GetErrorMessage());
+            promise.Reject(new Exception(request.GetErrorMessage()));
         }
+        return promise;
     }
 
-    static void downloadLevels(Action<List<LevelModelComponent>> onLevelsLoaded) {
+    static IPromise<List<LevelModelComponent>> downloadLevels() {
+        Promise<List<LevelModelComponent>> promise = new Promise<List<LevelModelComponent>>();
         GetLevels request = new GetLevels();
         builder.Build(request);
         while (!request.Process().isDone) { }
 
         if (request.Successful()) {
             request.ParseResult();
-            onLevelsLoaded(request.Levels);
+            promise.Resolve(request.Levels);
         }
         else {
-            onRequestFailed(request.GetErrorMessage());
+            promise.Reject(new Exception(request.GetErrorMessage()));
         }
+        return promise;
     }
 
-    static void downloadEnemies(Action<List<EnemyModelComponent>> onEnemiesLoaded) {
+    static IPromise<List<EnemyModelComponent>> downloadEnemies() {
+        Promise<List<EnemyModelComponent>> promise = new Promise<List<EnemyModelComponent>>();
         GetEnemies request = new GetEnemies();
         builder.Build(request);
         while (!request.Process().isDone) { }
 
         if (request.Successful()) {
             request.ParseResult();
-            onEnemiesLoaded(request.Enemies);
+            promise.Resolve(request.Enemies);
         }
         else {
-            onRequestFailed(request.GetErrorMessage());
+            promise.Reject(new Exception(request.GetErrorMessage()));
         }
+        return promise;
     }
 
-    static void saveLevels(List<LevelModelComponent> levels) {
+    static IPromise<List<BonusModelComponent>> downloadBonuses() {
+        Promise<List<BonusModelComponent>> promise = new Promise<List<BonusModelComponent>>();
+        GetBonuses request = new GetBonuses();
+        builder.Build(request);
+        while (!request.Process().isDone) { }
+
+        if (request.Successful()) {
+            request.ParseResult();
+            promise.Resolve(request.Bonuses);
+        }
+        else {
+            promise.Reject(new Exception(request.GetErrorMessage()));
+        }
+        return promise;
+    }
+
+    static IPromise<List<DifficultyModelComponent>> downloadDifficulties() {
+        Promise<List<DifficultyModelComponent>> promise = new Promise<List<DifficultyModelComponent>>();
+        GetDifficulties request = new GetDifficulties();
+        builder.Build(request);
+        while (!request.Process().isDone) { }
+
+        if (request.Successful()) {
+            request.ParseResult();
+            promise.Resolve(request.Difficulties);
+        }
+        else {
+            promise.Reject(new Exception(request.GetErrorMessage()));
+        }
+        return promise;
+    }
+
+    static IPromise saveLevels(List<LevelModelComponent> levels) {
+        Promise promise = new Promise();
         foreach (LevelModelComponent level in levels)
             Utils.Serialize(level, level.name);
+        promise.Resolve();
+        return promise;
     }
 
-    static void savePaths(List<PathModelComponent> paths) {
+    static IPromise savePaths(List<PathModelComponent> paths) {
+        Promise promise = new Promise();
         foreach (PathModelComponent path in paths)
             Utils.Serialize(path, path.name);
+        promise.Resolve();
+        return promise;
     }
 
-    static void saveEnemies(List<EnemyModelComponent> enemies) {
+    static IPromise saveEnemies(List<EnemyModelComponent> enemies) {
+        Promise promise = new Promise();
         foreach (EnemyModelComponent enemy in enemies)
             Utils.Serialize(enemy, enemy.type.ToString());
+        promise.Resolve();
+        return promise;
     }
 
-    static void onRequestFailed(string message) {
-        Debug.Log(message);
+    static IPromise saveDifficulties(List<DifficultyModelComponent> difficulties) {
+        Promise promise = new Promise();
+        foreach (DifficultyModelComponent difficulty in difficulties)
+            Utils.Serialize(difficulty, difficulty.type.ToString());
+        promise.Resolve();
+        return promise;
+    }
+
+    static IPromise saveBonuses(List<BonusModelComponent> bonuses) {
+        Promise promise = new Promise();
+        foreach (BonusModelComponent bonus in bonuses)
+            Utils.Serialize(bonus, bonus.type.ToString());
+        promise.Resolve();
+        return promise;
     }
 }
