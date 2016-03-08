1f18b15ead624170c83df22d5de0f9b6f6499739
Mateusz Debski <mateuszdeb@gmail.com>
2016-03-08 19:50:17 +0100

    feat: FacadeTool
    Fast facade change via Unity

---
+++ Assets/Editor/SwitchFacadeTool.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class SwitchFacadeTool
+{
+    const string facadePath = "/Script/Controller/Facade/Facade.cs";
+
+    [MenuItem("Jelly/Facade/Locale")]
+    public static void SetLocalFacade()
+    {
+        replaceTextInFacadeFile("HerokuFacadeParameters", "LocalFacadeParameters");
+    }
+
+    [MenuItem("Jelly/Facade/Heroku")]
+    public static void SetHerokuFacade()
+    {
+        replaceTextInFacadeFile("LocalFacadeParameters", "HerokuFacadeParameters");
+    }
+
+    static void replaceTextInFacadeFile(string from, string to)
+    {
+        string path = Application.dataPath + facadePath;
+        string content = File.ReadAllText(path).Replace(from, to);
+        File.WriteAllText(path, content);
+    }
+}
